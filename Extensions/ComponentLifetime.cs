945ff73c0fe73823443b6c6c057d27fcc21b3995
AroNixBox <nixonbusinessmail@gmail.com>
2025-03-04 20:18:08 +0100

    ADDED: Component Lifetime Component

---
+++ Extensions/ComponentLifetime.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Extensions {
+    public class ComponentLifetime : MonoBehaviour {
+        [SerializeField] float lifetime = 1f;  
+        [SerializeField] MonoBehaviour[] componentsToDisable;
+        [SerializeField] bool disableFromEvent;
+        
+        void Start() {
+            if (disableFromEvent) { return; }
+            Invoke(nameof(DisableComponents), lifetime);
+        }
+        
+        public void DisableComponents() {
+            foreach (var component in componentsToDisable) {
+                component.enabled = false;
+            }
+             
+            enabled = false;
+        }
+    }
+}
