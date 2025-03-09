46aeb2249b624cc83094d33815081888b7caee4c
AroNixBox <nixonbusinessmail@gmail.com>
2025-03-09 13:00:51 +0100

    FIXED: RotationState saving trigger & bail out

--- Extensions/RotationState.cs
+++ Extensions/RotationState.cs
@@ -5,12 +5,16 @@ namespace Extensions {
     public class RotationState : MonoBehaviour {
         [SerializeField] Vector3 endRotation = new(0, 90, 0);
         [SerializeField] UnityEvent rotationStateChange;
+        bool _triggered;
 
         public void TriggerRotation(bool opened) {
             if(!opened) { return; }
             
+            if(_triggered) { return; }
             transform.Rotate(endRotation);
             rotationStateChange?.Invoke();
+            
+            _triggered = true;
         }
     }
 }
