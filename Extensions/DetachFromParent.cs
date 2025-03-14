e7726b419acfa5c285182223aa625aafab727de6
AroNixBox <nixonbusinessmail@gmail.com>
2025-03-14 01:22:18 +0100

    UPDATED: DetatchFromParent Functionality

--- Extensions/DetachFromParent.cs
+++ Extensions/DetachFromParent.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 namespace Extensions {
     public class DetachFromParent : MonoBehaviour {
+        [SerializeField] bool detachFromParentOnStart = true;
         void Start() {
+            if (detachFromParentOnStart) {
+                Detach();
+            }
+        }
+
+        public void Detach() {
             transform.SetParent(null);
         }
     }
